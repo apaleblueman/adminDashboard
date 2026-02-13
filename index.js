7b45a296e534935a5d6eb6acb2a8597c82bd43e2
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-02-13 12:01:14 +0530

    ADD: basic layout usng flexbox and grid

---
+++ index.js
@@ -0,0 +1,8 @@
+// function create_element(parent, child, content){
+//     const parent_elem = document.getElementById(parent);
+//     const child_elem = document.createElement(child);
+//     child_elem.textContent = content;
+//     parent_elem.append(child_elem);
+
+// };
+
