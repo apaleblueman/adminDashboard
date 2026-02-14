da62a00b2ffe394ac5c6317296cc8b3b6f7625f2
Gurchetan Singh <gurchetan.singh2@icloud.com>
2026-02-14 19:51:41 +0530

    ADD: theme toggle functionality

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Admin Dashboard project from TheOdinProject
 
 <img src="./Screenshot.png" width=700px height=auto>
+
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-// function create_element(parent, child, content){
-//     const parent_elem = document.getElementById(parent);
-//     const child_elem = document.createElement(child);
-//     child_elem.textContent = content;
-//     parent_elem.append(child_elem);
+const toggleButton = document.getElementById('toggle');
 
-// };
+const savedTheme = localStorage.getItem('theme');
+if(savedTheme == 'dark'){
+    document.body.classList.toggle('dark-mode');
+    document.getElementById('toggle-icon').setAttribute('src', './assets/lightbulb-on.svg');
+}
+else{
+    document.body.classList.remove('dark-mode');
+    document.getElementById('toggle-icon').setAttribute('src', './assets/lightbulb-on.svg');
+}
 
+
+
+toggleButton.addEventListener('click', function(){        
+        if(document.body.getAttribute('class')== "dark-mode"){
+            document.body.classList.remove('dark-mode');
+            document.getElementById('toggle-icon').setAttribute('src', './assets/lightbulb-off.svg');
+            savedTheme = localStorage.setItem("theme","light");
+        }
+        else{
+            document.body.classList.toggle('dark-mode');
+            document.getElementById('toggle-icon').setAttribute('src', './assets/lightbulb-on.svg');
+            savedTheme = localStorage.setItem("theme","dark");
+        }
+   
+});
